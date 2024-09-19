7c34f2f76fe2331ed05972461c369e66917445f9
Keila <keilapagano@gmail.com>
2024-09-19 11:17:12 -0300

    start components

--- Practica/src/App.jsx
+++ Practica/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
-import Login from './components/login/Login'
+// import Login from './components/login/Login'
 
 function App() {
 
   return (
     <>
-      
-        <Login/>
-      
+      {/* <Login/>  */}
+        
     </>
   )
 }
