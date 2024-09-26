182e09a746fd366cea603f9ae2dd98cc04b401a3
Keila <keilapagano@gmail.com>
2024-09-25 23:33:15 -0300

    subo practica obligatoria 4

--- Practica/src/components/list/List.jsx
+++ Practica/src/components/list/List.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 
+
+
 const Lista = () => {
     const [items, setItems] = useState([]); 
     const [inputValue, setInputValue] = useState(''); 
+    const [checkedItems, setCheckedItems] = useState([]);
 
     const agregarTarea = () => {
         if (inputValue) { //si inputValue tiene un valor:
@@ -14,7 +17,15 @@ const Lista = () => {
     const borrarTarea = (index) => { 
         const newItems = items.filter((_, i) => i !== index);
         setItems(newItems);
+        setCheckedItems(checkedItems.filter((_, i) => i !== index));
+    };
 
+    const handleCheckTarea = (index) => {
+        setCheckedItems(prev => {
+            const newCheckedItems = [...prev];
+            newCheckedItems[index] = !newCheckedItems[index];
+            return newCheckedItems;
+        });
     };
 
     return (
@@ -22,19 +33,29 @@ const Lista = () => {
             <h3>Lista de tareas</h3>
             <input 
                 type="text" 
-                className="opacity-50"
-                value={inputValue} 
-                onChange={(e) => setInputValue(e.target.value)} 
+                className="opacity-50" 
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
             />
         
             <button type="button" className="btn btn-outline-primary, text-lg-end" onClick={agregarTarea}>Añadir tarea</button>
             
-            
+
             <ul className="text-start">
                 {items.map((item, index) => (
                     <li key={index}>
-                        {item}
+                        <input 
+                            type="checkbox"
+                            checked={!!checkedItems[index]}
+                            onChange={() => handleCheckTarea(index)}/>
+                            
+
+                            <span style={{ textDecoration: checkedItems[index] ? 'line-through' : 'none' }}>
+                                {item}
+                            </span>
+                        
                         <button onClick={() => borrarTarea(index)} className="btn btn-outline-secondary, text-lg-end">Borra tarea</button>
+                        
                     </li>
                 ))}
             </ul>
